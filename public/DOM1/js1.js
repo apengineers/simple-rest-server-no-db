e64b1ca042368cf96b69a81f11123da51e179ff4
apengineers <ap.engineers@gmail.com>
2015-08-18 09:38:34 -0400

    removed commented code

--- public/DOM1/js1.js
+++ public/DOM1/js1.js
@@ -2,6 +2,7 @@
 var b = document.getElementsByTagName("form");
 var c = document.getElementById("submit");
 c.addEventListener("click", stopDefAction);
+
 function stopDefAction(evt)
  {
  	document.getElementById("new").innerHTML= " ";
@@ -9,10 +10,8 @@ function stopDefAction(evt)
 	for(var i = 1; i<b[0].length; i++)
 	{	
 		var a = b[0];
-		//var a = document.getElementsByTagName("form");
 		var d = document.getElementById("new")
 		d.innerHTML  = d.innerHTML + " <br/>" + a[i-1].name + " " + a[i-1].value;
 	};	
-	//d.innerHTML = d.innerHTML + " <br/>" + document.getElementById("state").name + document.getElementById("state").value;
+	
 };
-//document.getElementById("new").innerHTML= " "
